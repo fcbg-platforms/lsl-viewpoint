73cc0e3bf1a931d5ec9cfc909d445dbf739d319f
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-11-08 16:44:32 +0100

    rm python 3.8 compat code

--- lsl_viewpoint/tests/test_config.py
+++ lsl_viewpoint/tests/test_config.py
@@ -1,6 +1,5 @@
 from __future__ import annotations  # post-poned evaluation of annotations
 
-import sys
 from typing import TYPE_CHECKING
 
 import pytest
@@ -8,22 +7,12 @@ import pytest
 from lsl_viewpoint import load_config, logger, set_log_level, write_config
 
 if TYPE_CHECKING:
-    from typing import Callable
+    pass
 
 logger.propagate = True
 set_log_level("WARNING")
 
 
-def requires_python39(function: Callable):
-    """Check if we have python 3.9 or above."""
-    if sys.version_info.major != 3:
-        raise RuntimeError("This program is designed for python 3.")
-    skip = sys.version_info.minor < 9
-    reason = "Requires python 3.9 for 'importlib.resources.files'."
-    return pytest.mark.skipif(skip, reason=reason)(function)
-
-
-@requires_python39
 def test_load_config():
     """Test configuration loader."""
     from importlib.resources import files
@@ -34,7 +23,6 @@ def test_load_config():
     assert sfreq == 60
 
 
-@requires_python39
 def test_write_config(tmp_path):
     """Test configuration writer."""
     from importlib.resources import files
@@ -52,7 +40,6 @@ def test_write_config(tmp_path):
     "fname",
     ("lsl-viewpoint-invalid-1", "lsl-viewpoint-invalid-2", "lsl-viewpoint-invalid-3"),
 )
-@requires_python39
 def test_load_invalid_config(caplog, fname):
     """Test loading of invalid configuration."""
     from importlib.resources import files
